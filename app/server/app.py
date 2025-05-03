935b6a740f2d4a8070dfbda2ff981f9a6f7c058e
Xazratov Behro'z <hazratqulovbehruz60@gmail.com>
2025-05-03 10:52:45 +0500

    added websocket with html template

--- app/server/app.py
+++ app/server/app.py
@@ -6,6 +6,7 @@ from app.api.views.rooms import router as rooms_router
 from app.api.views.auth import router as auth_router
 from app.api.views.booking import router as booking_router
 from app.api.views.rooms_type import router as rooms_type_router
+from app.api.views.websocket import app as websocket_router
 
 settings = get_settings()
 
@@ -20,5 +21,6 @@ def create_app() -> FastAPI:
     app_.include_router(auth_router, prefix="/auth")
     app_.include_router(booking_router, prefix="/booking")
     app_.include_router(rooms_type_router, prefix="/rooms_type")
+    app_.include_router(websocket_router, prefix="/websocket")
     return app_
 
